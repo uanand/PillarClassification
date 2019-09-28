0627c64ea4d3e9222e9a82a9c02e9a7335bc15b3
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-09-28 22:19:56 +0800

    Using the input parameters in excel file, generate a bunch of images which can be used as training dataset.

--- main/prepareCropImages.py
+++ main/prepareCropImages.py
@@ -1,9 +1,18 @@
+import os
+import sys
 import numpy
 import pandas
+import cv2
 import hyperspy.api as hs
 import matplotlib.pyplot as plt
+from skimage import transform
+from tqdm import tqdm
 
-outputDir = '/mnt/cbis/home/utkarsh/codes/pillarCollapseClassification/dataset/allImages'
+sys.path.append(os.path.abspath('../lib'))
+import imageProcess
+import imageDraw
+
+outputDir = '../dataset/allImages'
 
 ####################################################
 # READING THE EXCEL WORKBOOK FOR USER INPUTS
@@ -14,6 +23,8 @@ inputInfo = excelBook.parse(sheetName)
 ####################################################
 
 ####################################################
+counter = 1
+zfillVal = 6
 for line in inputInfo.values:
     inputFile = line[0]
     rowTopLeft = line[1]
@@ -26,5 +37,33 @@ for line in inputInfo.values:
     numPillarsInCol = line[8]
     cropSize = line[9]
     
+    rowBottomRight = rowBottomLeft-(rowTopLeft-rowTopRight)
+    colBottomRight = colBottomLeft-(colTopLeft-colTopRight)
     
-    rCenterList = 
+    f = hs.load(inputFile);
+    gImg = f.data
+    lowLimit = f.original_metadata['DocumentObjectList']['TagGroup0']['ImageDisplayInfo']['LowLimit']
+    highLimit = f.original_metadata['DocumentObjectList']['TagGroup0']['ImageDisplayInfo']['HighLimit']
+    gImg[gImg<lowLimit] = lowLimit
+    gImg[gImg>highLimit] = highLimit
+    gImg = imageProcess.normalize(gImg)
+    [row,col] = gImg.shape
+    
+    topRowPillarCentre = numpy.column_stack((\
+        numpy.linspace(rowTopLeft,rowTopRight,numPillarsInRow),\
+        numpy.linspace(colTopLeft,colTopRight,numPillarsInRow)))
+    bottomRowPillarCenter = numpy.column_stack((\
+        numpy.linspace(rowBottomLeft,rowBottomRight,numPillarsInRow),\
+        numpy.linspace(colBottomLeft,colBottomRight,numPillarsInRow)))
+        
+    for coordTop,coordBottom in tqdm(zip(topRowPillarCentre,bottomRowPillarCenter)):
+        pillarColumnCoord = numpy.column_stack((numpy.linspace(coordTop[0],coordBottom[0],numPillarsInCol),numpy.linspace(coordTop[1],coordBottom[1],numPillarsInCol)))
+        for r,c in pillarColumnCoord:
+            cropRowStart,cropColStart = int(round(r-cropSize/2)),int(round(c-cropSize/2))
+            cropRowEnd,cropColEnd = int(cropRowStart+cropSize),int(cropColStart+cropSize)
+            if (cropRowStart>=0 and cropColStart>=0 and cropRowEnd<=row and cropColEnd<=col):
+                gImgCrop = gImg[cropRowStart:cropRowEnd+1,cropColStart:cropColEnd+1]
+                gImgCrop = cv2.resize(gImgCrop,(64,64),interpolation=cv2.INTER_AREA)
+                cv2.imwrite(outputDir+'/'+str(counter).zfill(zfillVal)+'.png',gImgCrop)
+                counter += 1
+####################################################
