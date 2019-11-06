9cdc325f03a2a67fc43c5eb4687cca4f3b4b6ee0
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-11-06 17:21:43 +0800

    Helper function for flipping dataset.

--- lib/transform.py
+++ lib/transform.py
@@ -20,12 +20,31 @@ def rotateDataset(x,y,angle):
 
 
 #######################################################################
+# FLIP THE DATASET AROUND THE HORIZINTAL AND VERTICAL AXES
+#######################################################################
+def flipDataset(x,y):
+    [rowDataset,colDataset] = x.shape
+    row,col = int(numpy.sqrt(colDataset)),int(numpy.sqrt(colDataset))
+    xFlipHor,yFlipHor = x.copy(),y.copy(); xFlipHor[:] = 0;
+    xFlipVer,yFlipVer = x.copy(),y.copy(); xFlipVer[:] = 0;
+    for i in range(rowDataset):
+        gImg = numpy.reshape(x[i,:],(row,col))
+        gImgFlipHor = numpy.flip(gImg,axis=1)
+        gImgFlipVer = numpy.flip(gImg,axis=0)
+        xFlipHor[i,:] = gImgFlipHor.flatten()
+        xFlipVer[i,:] = gImgFlipVer.flatten()
+    xFlip = numpy.row_stack((xFlipHor,xFlipVer))
+    yFlip = numpy.concatenate((yFlipHor,yFlipVer))
+    return xFlip,yFlip
+#######################################################################
+
+
+#######################################################################
 # RESIZE THE TRAINING DATASET
 #######################################################################
 def resizeDataset(x,size):
     [rowDataset,colDataset] = x.shape
     row,col = int(numpy.sqrt(colDataset)),int(numpy.sqrt(colDataset))
-    # rowNew,colNew = size(0),size[1]
     xResize = numpy.zeros([rowDataset,size[0]*size[1]],dtype='uint8')
     for i in range(rowDataset):
         gImg = numpy.reshape(x[i,:],(row,col))
