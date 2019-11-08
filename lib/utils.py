d7b6379f3079d5ebaa1082317196c5eaa2cc0fa4
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-11-08 18:45:17 +0800

    Change best model selection criteria and keras release memory.

--- lib/utils.py
+++ lib/utils.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 def selectBestModel(modelFileList,xTrain,yTrainInd,xTest,yTestInd):
     bestAccuracyScore,bestLossScore = 0,1e10
+    # minDiffAccuracy,minDiffLoss = 1e10,1e10
     nTrain,nTest = xTrain.shape[0],xTest.shape[0]
     for modelFile in tqdm(modelFileList):
         model = keras.models.load_model(modelFile)
@@ -14,18 +15,24 @@ def selectBestModel(modelFileList,xTrain,yTrainInd,xTest,yTestInd):
         scoreLoss = (scoresTrain[0]+scoresTest[0])/2.0
         # scoreAccuracy = (scoresTrain[1]*nTrain+scoresTest[1]*nTest)/(nTrain+nTest)
         # scoreLoss = (scoresTrain[0]*nTrain+scoresTest[0]*nTest)/(nTrain+nTest)
-        if (scoreAccuracy>bestAccuracyScore):
-            bestAccuracyScore = scoreAccuracy
-            bestAccuracyModelFile = modelFile
-            print (modelFile,scoresTrain[1],scoresTest[1],scoreAccuracy,bestAccuracyScore)
-        if (scoreLoss<bestLossScore):
-            bestLossScore = scoreLoss
-            bestLossModelFile = modelFile
-            print (modelFile,scoresTrain[0],scoresTest[0],scoreLoss,bestLossScore)
+        diffAccuracy,diffLoss = numpy.abs(scoresTrain[1]-scoresTest[1]),numpy.abs(scoresTrain[0]-scoresTest[0])
+        if (scoreAccuracy-diffAccuracy>bestAccuracyScore):
+            # if (diffAccuracy<=minDiffAccuracy):
+                bestAccuracyScore = scoreAccuracy-diffAccuracy
+                # bestAccuracyScore = scoreAccuracy
+                bestAccuracyModelFile = modelFile
+                # minDiffAccuracy = diffAccuracy
+        if (scoreLoss+diffLoss<bestLossScore):
+            # if (diffLoss<=minDiffLoss):
+                bestLossScore = scoreLoss+diffLoss
+                # bestLossScore = scoreLoss
+                bestLossModelFile = modelFile
+                # minDiffLoss = diffLoss
+        # del model
+        keras.backend.clear_session()
             
     for modelFile in modelFileList:
         if not(modelFile==bestAccuracyModelFile or modelFile==bestLossModelFile):
-            print ('Removing',modelFile)
             os.remove(modelFile)
             
     modelAccuracy = keras.models.load_model(bestAccuracyModelFile)
