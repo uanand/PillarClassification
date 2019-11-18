f9a416243b1509d020a053be3cb660e41ed07f04
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-11-18 12:24:23 +0800

    Helper function for reading dm3 and dm4 files.

--- lib/imageProcess.py
+++ lib/imageProcess.py
@@ -1,9 +1,11 @@
 import numpy
 import cv2
-# from mahotas.polygon import fill_convexhull
+import hyperspy.api as hs
 from scipy import ndimage
 from skimage.morphology import disk, white_tophat
 from skimage import measure
+
+import imageProcess
 
 #######################################################################
 # NORMALIZE AN 8 BIT GRAYSCALE IMAGE
@@ -15,5 +17,19 @@ def normalize(gImg, min=0, max=255):
     elif (gImg.max() > 0):
         gImg[:] = max
     gImg=gImg.astype('uint8')
+    return gImg
+#######################################################################
+
+#######################################################################
+# READ GATAN DM3 OR DM4 FILES
+#######################################################################
+def readDM4(fileName):
+    f = hs.load(fileName);
+    gImg = f.data
+    lowLimit = f.original_metadata['DocumentObjectList']['TagGroup0']['ImageDisplayInfo']['LowLimit']
+    highLimit = f.original_metadata['DocumentObjectList']['TagGroup0']['ImageDisplayInfo']['HighLimit']
+    gImg[gImg<lowLimit] = lowLimit
+    gImg[gImg>highLimit] = highLimit
+    gImg = imageProcess.normalize(gImg)
     return gImg
 #######################################################################
