9d26c11bfdab2e6ed87ee3ef3692480ebf387a43
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-12-15 22:08:06 +0800

    Plotting helper functions.

--- notebooks/plotCollapsedPillars.py
+++ notebooks/plotCollapsedPillars.py
@@ -44,22 +44,57 @@ plt.style.use('UMM_ISO9001')
 ############################################################
 # PLOT FOR RESTORE EXPERIMENTS WITH WATER
 ############################################################
+# numSamples = 100
+# dataFile = '/home/utkarsh/Desktop/exSitu/Plasma+RestoreByWater/Plasma+RestoreByWater.dat'
+# df = pandas.read_csv(dataFile,delimiter='\t')
+# tags = [0,1]
+# percentStanding = {}
+# avgList,stdList = [],[]
+# for tag in tqdm(tags):
+    # data = df[df['Tag']==tag]['ClassificationClass']
+    # percentStanding[tag] = []
+    # for i in range(1000):
+        # percentStanding[tag].append(100.0*numpy.sum(shuffle(data)[:numSamples])/numSamples)
+    # avgList.append(numpy.mean(percentStanding[tag]))
+    # stdList.append(numpy.std(percentStanding[tag]))
+    
+# dataFile = '/home/utkarsh/Desktop/exSitu/RestoreByWater/RestoreByWater.dat'
+# df = pandas.read_csv(dataFile,delimiter='\t')
+# tags = numpy.unique(df['Tag'])
+# for tag in tqdm(tags):
+    # data = df[df['Tag']==tag]['ClassificationClass']
+    # percentStanding[tag] = []
+    # for i in range(1000):
+        # percentStanding[tag].append(100.0*numpy.sum(shuffle(data)[:numSamples])/numSamples)
+    # avgList.append(numpy.mean(percentStanding[tag]))
+    # stdList.append(numpy.std(percentStanding[tag]))
+    
+# fig = plt.figure(figsize=(2.8,2.0))
+# ax = fig.add_axes([0,0,1,1])
+# ax.fill_between((-1,2.5),y1=avgList[0]-stdList[0],y2=avgList[0]+stdList[0],ec='None',fc='r',alpha=0.25)
+# ax.axhline(avgList[0],c='r',label='water dry')
+# ax.errorbar(x=[0,1./6,2./6,3./6,1,2],y=avgList[1:],yerr=stdList[1:],c='b',marker='o',ls='None',capsize=2,label='dry box')
+# ax.set_xlim(-0.2,2.2)
+# ax.set_ylim(0,100)
+# ax.set_xticks([0,0.5,1,1.5,2])
+# ax.set_xlabel('t (h)')
+# ax.set_ylabel('% Standing NP')
+# ax.legend(numpoints=1)
+# plt.savefig('RestoreByWater.png',format='png')
+# plt.savefig('RestoreByWater.pdf',format='pdf')
+# plt.close()
+############################################################
+
+############################################################
+# PLOT FOR RESTORE EXPERIMENTS WITH 1.00% HF
+############################################################
 numSamples = 100
-dataFile = '/home/utkarsh/Desktop/exSitu/Plasma+RestoreByWater/Plasma+RestoreByWater.dat'
+dataFile = 'RestoreBy1.00%HF.dat'
 df = pandas.read_csv(dataFile,delimiter='\t')
 tags = [0,1]
 percentStanding = {}
 avgList,stdList = [],[]
-for tag in tqdm(tags):
-    data = df[df['Tag']==tag]['ClassificationClass']
-    percentStanding[tag] = []
-    for i in range(1000):
-        percentStanding[tag].append(100.0*numpy.sum(shuffle(data)[:numSamples])/numSamples)
-    avgList.append(numpy.mean(percentStanding[tag]))
-    stdList.append(numpy.std(percentStanding[tag]))
-    
-dataFile = '/home/utkarsh/Desktop/exSitu/RestoreByWater/RestoreByWater.dat'
-df = pandas.read_csv(dataFile,delimiter='\t')
+
 tags = numpy.unique(df['Tag'])
 for tag in tqdm(tags):
     data = df[df['Tag']==tag]['ClassificationClass']
@@ -69,18 +104,83 @@ for tag in tqdm(tags):
     avgList.append(numpy.mean(percentStanding[tag]))
     stdList.append(numpy.std(percentStanding[tag]))
     
-fig = plt.figure(figsize=(2.8,2.0))
+fig = plt.figure(figsize=(2.8,1.0))
 ax = fig.add_axes([0,0,1,1])
-ax.fill_between((-1,2.5),y1=avgList[0]-stdList[0],y2=avgList[0]+stdList[0],ec='None',fc='r',alpha=0.25)
-ax.axhline(avgList[0],c='r',label='water dry')
-ax.errorbar(x=[0,1./6,2./6,3./6,1,2],y=avgList[1:],yerr=stdList[1:],c='b',marker='o',ls='None',capsize=2,label='dry box')
-ax.set_xlim(-0.2,2.2)
+ax.errorbar(x=[0,10,20,30,40,50,60],y=avgList,yerr=stdList,c='g',marker='o',ls='None',capsize=2,label='1.00% HF')
+ax.set_xlim(-10,310)
 ax.set_ylim(0,100)
-ax.set_xticks([0,0.5,1,1.5,2])
-ax.set_xlabel('t (h)')
+ax.set_xticks([0,30,60,90,120,150,180,210,240,270,300])
+ax.set_xlabel('t (s)')
 ax.set_ylabel('% Standing NP')
-ax.legend(numpoints=1)
-plt.savefig('RestoreByWater.png',format='png')
-plt.savefig('RestoreByWater.pdf',format='pdf')
+# ax.legend(numpoints=1)
+plt.savefig('RestoreBy1.00%HF.png',format='png')
+plt.savefig('RestoreBy1.00%HF.pdf',format='pdf')
+plt.close()
+############################################################
+
+############################################################
+# PLOT FOR RESTORE EXPERIMENTS WITH 0.10% HF
+############################################################
+numSamples = 100
+dataFile = 'RestoreBy0.10%HF.dat'
+df = pandas.read_csv(dataFile,delimiter='\t')
+tags = [0,1]
+percentStanding = {}
+avgList,stdList = [],[]
+
+tags = numpy.unique(df['Tag'])
+for tag in tqdm(tags):
+    if not(tag==2 or tag==4):
+        data = df[df['Tag']==tag]['ClassificationClass']
+        percentStanding[tag] = []
+        for i in range(1000):
+            percentStanding[tag].append(100.0*numpy.sum(shuffle(data)[:numSamples])/numSamples)
+        avgList.append(numpy.mean(percentStanding[tag]))
+        stdList.append(numpy.std(percentStanding[tag]))
+    
+fig = plt.figure(figsize=(2.8,1.0))
+ax = fig.add_axes([0,0,1,1])
+ax.errorbar(x=[0,10,30,50,60,120,300],y=avgList,yerr=stdList,c='g',marker='o',ls='None',capsize=2,label='1.00% HF')
+ax.set_xlim(-10,310)
+ax.set_ylim(0,100)
+ax.set_xticks([0,30,60,90,120,150,180,210,240,270,300])
+ax.set_xlabel('t (s)')
+ax.set_ylabel('% Standing NP')
+# ax.legend(numpoints=1)
+plt.savefig('RestoreBy0.10%HF.png',format='png')
+plt.savefig('RestoreBy0.10%HF.pdf',format='pdf')
+plt.close()
+############################################################
+
+############################################################
+# PLOT FOR RESTORE EXPERIMENTS WITH 0.30% HF
+############################################################
+numSamples = 100
+dataFile = 'RestoreBy0.30%HF.dat'
+df = pandas.read_csv(dataFile,delimiter='\t')
+tags = [0,1]
+percentStanding = {}
+avgList,stdList = [],[]
+
+tags = numpy.unique(df['Tag'])
+for tag in tqdm(tags):
+    data = df[df['Tag']==tag]['ClassificationClass']
+    percentStanding[tag] = []
+    for i in range(1000):
+        percentStanding[tag].append(100.0*numpy.sum(shuffle(data)[:numSamples])/numSamples)
+    avgList.append(numpy.mean(percentStanding[tag]))
+    stdList.append(numpy.std(percentStanding[tag]))
+    
+fig = plt.figure(figsize=(2.8,1.0))
+ax = fig.add_axes([0,0,1,1])
+ax.errorbar(x=[0,30,60,90,120],y=avgList,yerr=stdList,c='g',marker='o',ls='None',capsize=2,label='1.00% HF')
+ax.set_xlim(-10,310)
+ax.set_ylim(0,100)
+ax.set_xticks([0,30,60,90,120,150,180,210,240,270,300])
+ax.set_xlabel('t (s)')
+ax.set_ylabel('% Standing NP')
+# ax.legend(numpoints=1)
+plt.savefig('RestoreBy0.30%HF.png',format='png')
+plt.savefig('RestoreBy0.30%HF.pdf',format='pdf')
 plt.close()
 ############################################################
--- notebooks/plotHistory.py
+++ notebooks/plotHistory.py
@@ -1,7 +1,10 @@
 import numpy
 import matplotlib.pyplot as plt
+plt.style.use('UMM_ISO9001')
 
-inputFile = '/home/utkarsh/Projects/PillarClassification/model/model_01_epochs_500_batchsize_1000_trainAcc_99.65_testAcc_99.60.dat'
+inputFile = '/home/utkarsh/Projects/PillarClassification/model/model_01_newData_epochs_500_batchsize_1000_trainAcc_98.33_testAcc_98.33.dat'
+fileName = inputFile.replace('.dat','.png')
+
 f = open(inputFile,'r')
 text = f.read()
 text = text.split("{'loss': [")[1]
@@ -24,19 +27,20 @@ val_accuracy = numpy.asarray(val_accuracy,dtype='double')
 fig = plt.figure(figsize=(8,4))
 ax1 = fig.add_subplot(121)
 ax2 = fig.add_subplot(122)
-ax1.plot(x,lossTrain,label='train')
-ax1.plot(x,lossTest,label='test')
+ax1.plot(epochs,loss,label='train')
+ax1.plot(epochs,val_loss,label='test')
 ax1.set_xlabel('Iterations')
 ax1.set_ylabel('Loss')
-ax1.set_xlim(min(x),max(x))
-ax1.set_ylim(min(min(lossTrain),min(lossTest)),max(max(lossTrain),max(lossTest)))
+ax1.set_xlim(min(epochs),max(epochs))
+ax1.set_ylim(min(min(loss),min(val_loss)),max(max(loss),max(val_loss)))
 ax1.legend()
-ax2.plot(x,accuracyTrain,label='train')
-ax2.plot(x,accuracyTest,label='test')
+ax2.plot(epochs,accuracy,label='train')
+ax2.plot(epochs,val_accuracy,label='test')
 ax2.set_xlabel('Iterations')
 ax2.set_ylabel('Accuracy')
-ax2.set_xlim(min(x),max(x))
-ax2.set_ylim(min(min(accuracyTrain),min(accuracyTest)),max(max(accuracyTrain),max(accuracyTest)))
+ax2.set_xlim(min(epochs),max(epochs))
+ax2.set_ylim(0.95,1)
+# ax2.set_ylim(min(min(accuracy),min(val_accuracy)),max(max(accuracy),max(val_accuracy)))
 ax2.legend()
 plt.savefig(fileName,format='png')
 plt.close()
