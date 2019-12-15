87152b39dc32cb4a468bfe25ab32e2494318c1d8
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-12-15 22:03:58 +0800

    No change.

--- main/prepareTrainingDataset.py
+++ main/prepareTrainingDataset.py
@@ -78,7 +78,7 @@ def writeClassificationToFile(gImgStack,targetList,counter,outputFile):
 
 
 ############################################################
-# 
+# REMOVE DUPLICATES FROM THE TRAINING DATASET 
 ############################################################
 def removeDuplicates(outputFile):
     print("Removing duplicates from the labelled dataset.")
@@ -175,29 +175,3 @@ submit.on_clicked(updateLabel)
 skip.on_clicked(loadNewImage)
 exitSoftware.on_clicked(quitProgram)
 ############################################################
-
-# import sys
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-
-# def press(event):
-    # print('press', event.key)
-    # sys.stdout.flush()
-    # if event.key == 'x':
-        # visible = xl.get_visible()
-        # xl.set_visible(not visible)
-        # fig.canvas.draw()
-
-# # Fixing random state for reproducibility
-# np.random.seed(19680801)
-
-
-# fig, ax = plt.subplots()
-
-# fig.canvas.mpl_connect('key_press_event', press)
-
-# ax.plot(np.random.rand(12), np.random.rand(12), 'go')
-# xl = ax.set_xlabel('easy come, easy go')
-# ax.set_title('Press a key')
-# plt.show()
