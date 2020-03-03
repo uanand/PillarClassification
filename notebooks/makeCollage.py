dacc7dd388a19a4c5cad72a8fc3e492262a00add
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-03-03 14:04:22 +0800

    Update makeCollage.py

--- notebooks/makeCollage.py
+++ notebooks/makeCollage.py
@@ -1,39 +1,47 @@
+import os
+import sys
 import cv2
 import numpy
-import random
 from skimage.transform import rotate
+from sklearn.utils import shuffle
 
-# inputDir = '../dataset/allImages'
-# firstFrame,lastFrame = 1,1762
-# zfillVal = 6
-# frameList = list(range(firstFrame,lastFrame+1))
-# random.shuffle(frameList)
+sys.path.append(os.path.abspath('../lib'))
+import imageProcess
 
-# counter = 0
-# for frame1 in range(8):
-    # for frame2 in range(16):
-        # gImg = cv2.imread(inputDir+'/'+str(frameList[counter]).zfill(zfillVal)+'.png',0)
-        # counter+=1
-        # try:
-            # columnImg = numpy.column_stack((columnImg,gImg))
-        # except:
-            # columnImg = gImg.copy()
-    # try:
-        # rowImg = numpy.row_stack((rowImg,columnImg))
-    # except:
-        # rowImg = columnImg.copy()
-    # del columnImg
-# cv2.imwrite('collage.png',rowImg)
 
+data = numpy.loadtxt('../dataset/labelledDataset.dat',skiprows=1)
+data = shuffle(data,random_state=10)
+x,y = data[:,1:],data[:,0]
+[row,col] = x.shape
+
+imgSize = 64
+numRowCollage,numColCollage = 3,20
+counter_0,counter_1 = 0,0
+img_0 = numpy.zeros([imgSize*numRowCollage,imgSize*numColCollage],dtype='uint8')
+img_1 = numpy.zeros([imgSize*numRowCollage,imgSize*numColCollage],dtype='uint8')
+for i in range(row):
+    if (y[i]==0 and counter_0<numRowCollage*numColCollage):
+        row_0,col_0 = int(int(counter_0/numColCollage)*imgSize),int((counter_0%numColCollage)*imgSize)
+        counter_0+=1
+        img = numpy.reshape(x[i,:],(64,64))
+        img = imageProcess.normalize(img)
+        img_0[row_0:row_0+imgSize,col_0:col_0+imgSize] = img
+    elif (y[i]==1 and counter_1<numRowCollage*numColCollage):
+        row_1,col_1 = int(int(counter_1/numColCollage)*imgSize),int((counter_1%numColCollage)*imgSize)
+        counter_1+=1
+        img = numpy.reshape(x[i,:],(64,64))
+        img = imageProcess.normalize(img)
+        img_1[row_1:row_1+imgSize,col_1:col_1+imgSize] = img
+    if (counter_0>=numRowCollage*numColCollage and counter_1>=numRowCollage*numColCollage):
+        break
+cv2.imwrite('collage_collapsed.png',img_1)
+cv2.imwrite('collage_not_collapsed.png',img_0)
 
 ###############################
 # ROTATE AND CONCATENATE IMAGES
-gImg = cv2.imread('/home/utkarsh/Projects/PillarClassification/dataset/allImages/002475.png',0)
-gImg_90 = (rotate(gImg,90)*255).astype('uint8')
-gImg_180 = (rotate(gImg,180)*255).astype('uint8')
-gImg_270 = (rotate(gImg,270)*255).astype('uint8')
-finalImg = numpy.column_stack((gImg,gImg_90,gImg_180,gImg_270))
-cv2.imwrite('image.png',finalImg)
-
-
-# utils.imagesForLabelDataset(fileName='../dataset/labelledDataset.dat',numClasses=2,dirList=['/home/utkarsh/Projects/PillarClassification/dataset/labelledImages/Collapse','/home/utkarsh/Projects/PillarClassification/dataset/labelledImages/Not collapse'])
+# gImg = cv2.imread('/home/utkarsh/Projects/PillarClassification/dataset/allImages/002475.png',0)
+# gImg_90 = (rotate(gImg,90)*255).astype('uint8')
+# gImg_180 = (rotate(gImg,180)*255).astype('uint8')
+# gImg_270 = (rotate(gImg,270)*255).astype('uint8')
+# finalImg = numpy.column_stack((gImg,gImg_90,gImg_180,gImg_270))
+# cv2.imwrite('image.png',finalImg)
