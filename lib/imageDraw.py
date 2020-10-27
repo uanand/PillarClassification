be43a6552917f140dbcdf15ee5455ab675e48e0f
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-10-27 09:26:33 +0800

    Docstring for circle in imageDraw.py

--- lib/imageDraw.py
+++ lib/imageDraw.py
@@ -1,11 +1,29 @@
 import cv2
 import numpy
-import matplotlib.pyplot as plt
 
 ############################################################
 # DRAW A CIRCLE IN AN IMAGE
 ############################################################
 def circle(img,center,radius,thickness=1,color=255):
+    '''
+    Draw circle on an image.
+    
+    Input parameters:
+    img : (uint8 2D array) Image numpy array in grayscale
+    centre : (tuple/list) Centre coordinates of the circle as a tuple
+        (row,column) or a list [row,column].
+    radius : (int) Radius of the circle in pixels.
+    thickness : (int) Thickness of the circle boundary. If 1, then a 1
+        pixel thick circle is drawn on the image. If set to a negative
+        value, a filled circle is drawn.  
+    color : (int) Grayscale intensity of the circle. 0 makes a black
+        circle, 255 makes a white circle. For an intemediate grayscale
+        value choose a number between 0 and 255.
+        
+    Returns:
+    img : (uint8 2D array) Image numpy array with circle drawn based on
+        the input parameters.
+    '''
     [row,col] = img.shape
     img = cv2.circle(img,center=(int(center[1]),int(center[0])),radius=radius,color=color,thickness=thickness)
     return img
